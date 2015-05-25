daaafc6125bf1b2876b195f67689ea70cf168370
luixlive <luixlive@gmail.com>
2015-05-25 16:40:27 -0500

    Se añade mapa en el tercer fragment del pageadapter. Para esto se añaden varios permisos al manifest y el google key. -Luis Alfonso

--- app/src/main/java/com/ramon/umg/ramon/Fragment3.java
+++ app/src/main/java/com/ramon/umg/ramon/Fragment3.java
@@ -1,11 +1,21 @@
 package com.ramon.umg.ramon;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 
 /**
@@ -16,10 +26,48 @@ import android.view.ViewGroup;
  * Use the {@link Fragment3#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment3 extends Fragment {
+public class Fragment3 extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap.OnMyLocationChangeListener LocationCListener;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fragment3, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment3, container, false);
+        FragmentoMapa mapFragment = new FragmentoMapa();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.map, mapFragment).commit();
+        mapFragment.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        final GoogleMap map2 = map;
+        map.setMyLocationEnabled(true);
+
+        LocationManager locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(provider);
+        LatLng loc;
+        if(location!=null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            loc = new LatLng(latitude, longitude);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 15));
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+        }
+
+        LocationCListener = new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+                if(map2 != null){
+                    map2.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 15));
+                    map2.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+                }
+            }
+        };
     }
 }
